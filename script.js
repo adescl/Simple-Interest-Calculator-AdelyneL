6ec9e8ce6174526a35a00b77983de873bb98fc44
adescl <103479299+adescl@users.noreply.github.com>
2022-04-12 17:51:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function init() {
     
+    /* declare variables*/
+    amount = document.getElementById('amount');
+    rate = document.getElementById('rate_slider');
+    rate_display = document.getElementById('rate_display');
+    nb_years=document.getElementById('nbyears');
+    result=document.getElementById('result');
+    
+    // display current rate
+    update_rate(rate.value);
+
+    // reset output
+    result.innerHTML = ""
+
+    // focus on first input field
+    amount.focus();
 }
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+
+
+function update_rate(val) {
+    rate_display.innerHTML = parseFloat(val).toFixed(2) + "%" ; 
 }
-document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+
+function isAmountOK(val){
+    return (isNaN(val) || (parseInt(val) <= 0) || val === undefined || val == null || val.length <= 0) ? false : true;
+}
+
+// enable button only if amount is a positive number
+function validate() { 
+    if (isNaN(amount.value) || isEmpty(amount.value) || (parseInt(amount.value) <= 0)) { 
+        alert("Positive amount expected"); 
+
+    }
+}
+
+
+
+function compute() {
+
+    if (isAmountOK(amount.value) == true) { 
+        var today= new Date();
+        var cur_year = today.getFullYear();
+        nbyears = parseInt(nb_years.value)
+        interest = amount.value * rate.value * nbyears /100;
+        sht = "<span class='highlight'>"
+        eht = "</span>"
+        result.innerHTML =  "<br/> <br/>" + "If you deposit " + sht + amount.value + eht + ",<br/>" +
+                            "at an interest rate of " + sht + rate_display.innerHTML + eht + ".<br/>" +
+                            "You will receive an amount of " + sht + interest + eht + ",<br/>" +
+                            "in the year " + sht + (cur_year + nbyears) + eht;
+    }
+    else  { 
+        result.innerHTML = "";
+        amount.focus();
+        alert("Positive amount expected"); 
+    }
+}

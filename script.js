708fa8dc8bddd1d2d4b95a5c903780c7e0076f9e
adescl <103479299+adescl@users.noreply.github.com>
2022-04-12 18:45:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function compute()
         document.getElementById("principal").focus();
         return;
     }
-
+//variables
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
@@ -17,7 +17,7 @@ function compute()
     var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
     
     var resultDisplay = document.getElementById("result");
-    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+    resultDisplay.innerHTML = "If you deposit " + "<span class='highlight:yellow'>" + principal + "</span>."  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
 function SliderValue()
